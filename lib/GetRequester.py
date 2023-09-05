cf6212a5486d60bb96ff1d05fe2af3840d0746fb
Harpreet Singh <Harpreets1201@gmail.com>
2023-09-04 21:50:44 -0400

    Lab completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,16 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
+
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        people_list = []
+        peoples = json.loads(self.get_response_body())
+        for person in peoples:
+            people_list.append(person)
+
+        return people_list
+
